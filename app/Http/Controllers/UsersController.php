96c348f4f3ed8143f3205306b30ccda0471821fa
Teepluss <teepluss@gmail.com>
2016-11-27 20:22:03 +0700

    example

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('api');
+        $this->middleware('auth:api')->except(['index']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'     => 'required',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required'
+        ]);
+
+        $user = User::create($request->all());
+
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+
+        return response()->json($user, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name'     => 'sometimes|required'
+        ]);
+
+        $user = User::findOrFail($id);
+        $user->fill($request->all())->save();
+
+        return response()->json($user, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json([], 204);
+    }
+
+    public function credit($id)
+    {
+        $user = User::findOrFail($id);
+
+        return response()->json(['remainings' => 1000]);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+1. Reanme .env.example to .env
+2. Config .env
+3. Run CLI "composer install"
+3. Run CLI "php artisan migrate"
+4. Run CLI "php artisan passport:install"
